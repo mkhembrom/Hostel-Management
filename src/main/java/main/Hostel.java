c9f39e2b49139b19a2b931cb3ececdc6fee7bd4b
M.K.Hembrom <M.K.Hembrom@DESKTOP-PNH0NHN>
2019-05-12 14:14:15 +0530

    Completed Successfully

--- src/main/java/main/Hostel.java
+++ src/main/java/main/Hostel.java
@@ -41,16 +41,28 @@ public class Hostel{
 		               String checkName = splited[0];
 		               //write your code here !!!
 //		               compare check name with name and return true if present and false if not
+		               if(splited[0].equals(name))
+		               {
+		            	   return true;
+		               }
+
 	               }
 	               
 	               
 	            }catch(Exception e){
 	                System.out.println(e);
 	            }
-			return true;
+			return false;
 	   }
        public static void allotHostel(){
     	   //write your code here!!!
+    	   try
+    	   {
+    		   writedata();
+    	   }catch(IOException e)
+    	   {
+    		   System.out.println(e);
+    	   }
     	   
        }
 
@@ -76,7 +88,7 @@ public class Hostel{
     	   boolean chk = true;
     	   
     	   //write your code here
-    	   
+    	   chk = readData(name);
     	   return chk;
         }
         
